ae73aed49da74a71b0f062adc98a099d1494e95e
Leonardo Vales <leonardo.vales@sistemafaculdade.com.br>
2020-04-20 11:22:44 -0300

    Iniciando a tela de autenticação

---
+++ Task.js
@@ -0,0 +1,189 @@
+import React, { Component } from 'react';
+import { View, 
+         Text,
+         ImageBackground,
+         StyleSheet,
+         FlatList,
+         TouchableOpacity,
+         Platform,
+         StatusBar,
+         Alert,
+         AsyncStorage
+        } from 'react-native';
+
+// import AsyncStorage from '@react-native-community/async-storage'
+import todayImage from './assets/imgs/today.jpg';
+import moment from 'moment'
+import 'moment/locale/pt-br'
+import commonStyles from './src/commonStyles'
+import Task from './src/components/Task'
+import { Ionicons } from '@expo/vector-icons';
+import AddTask from './src/screens/AddTask'    
+
+
+const initialState = {
+    showDoneTasks: true,
+    showAddTask:   false,
+    visibleTasks:  [],
+    tasks:         []
+}
+
+export default class Task extends Component {
+
+  state = {
+    ...initialState
+  }
+
+  componentDidMount = async () => {
+    const stateString = await AsyncStorage.getItem('tasksState')
+    const state = JSON.parse(stateString) || initialState
+    this.setState(state, this.filterTasks)
+  }
+
+  toggleFilter = () => {
+    this.setState({showDoneTasks: !this.state.showDoneTasks}, this.filterTasks)
+  }
+
+  filterTasks = () => {
+    let visibleTasks = null;
+    if (this.state.showDoneTasks) {
+      visibleTasks = [...this.state.tasks]
+    } else {
+      const pending = task => task.doneAt === null
+      visibleTasks = this.state.tasks.filter(pending)
+    }
+    this.setState({visibleTasks})
+    AsyncStorage.setItem('tasksState', JSON.stringify(this.state))
+  }
+
+  toggleTask = taskId => {
+
+    const tasks = [...this.state.tasks]
+    tasks.forEach(task => {
+      if (task.id === taskId) {
+        task.doneAt = task.doneAt ? null : new Date();
+      }
+    })
+
+    this.setState({tasks: tasks}, this.filterTasks)
+
+  }
+
+  AddTask = newTask => {
+    
+    if (!newTask.desc || !newTask.desc.trim()) {
+      Alert.alert('Dados inválidos', 'Descrição não informada')
+      return 
+    }
+
+    const tasks = [...this.state.tasks]
+    tasks.push({
+      id:         Math.random(),
+      desc:       newTask.desc,
+      estimateAt: newTask.date,
+      doneAt:     null
+    })
+
+    this.setState({tasks, showAddTask: false}, this.filterTasks)
+
+  }
+
+  deleteTask = id => {
+    const tasks = this.state.tasks.filter(task => task.id !== id) //Pega todas as takks que são diferentes do ID passado
+    this.setState({ tasks }, this.filterTasks) //e gera um novo array atualizando o status
+  }
+
+  render() {
+
+    const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
+
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <AddTask 
+          isVisible={this.state.showAddTask} 
+          onCancel={() => this.setState({showAddTask: false})}
+          onSave={this.AddTask}  />
+        <ImageBackground source={todayImage} style={styles.background}>
+          
+            <TouchableOpacity style={styles.iconBar} onPress={this.toggleFilter}>
+              
+                <Ionicons 
+                  name={this.state.showDoneTasks ? 'md-eye' : 'md-eye-off'}                   
+                  size={40} 
+                  color={commonStyles.colors.secondary} />
+            
+            </TouchableOpacity>
+          
+          <View style={styles.titleBar}>
+            <Text style={styles.title}>Hoje</Text>
+            <Text style={styles.subtitle}>{today}</Text>
+          </View>
+        </ImageBackground>
+        <View style={styles.taskList}>
+          <FlatList 
+            data={this.state.visibleTasks}
+            keyExtractor={item => `${item.id}`}
+            renderItem={({item}) => <Task {...item} toggleTask={this.toggleTask} onDelete={this.deleteTask} />}
+          />
+        </View>   
+        <TouchableOpacity 
+          style={styles.addButton}
+          onPress={() => this.setState({showAddTask: true})}
+          activeOpacity={0.7}>
+          <Ionicons 
+            name='md-add' 
+            size={20} 
+            color={commonStyles.colors.secondary} />
+        </TouchableOpacity> 
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  background: {
+    flex: 3, //30% da tela
+  },
+  taskList: {
+    flex: 7, //70% da tela
+  },
+  titleBar: {
+    flex: 1, //quando eu coloco 1, estou dizendo que vou permitir que ele cresça dentro do escopo 
+    justifyContent: 'flex-end',
+  },
+  title: {
+    fontSize: 50,
+    color: commonStyles.colors.secondary,
+    marginLeft: 20,
+    marginBottom: 20
+  },
+  subtitle: {
+    fontSize: 20,
+    color: commonStyles.colors.secondary,
+    marginLeft: 20,
+    marginBottom: 30
+  },
+  iconBar: {
+    flexDirection: 'row',
+    marginHorizontal: 20,
+    
+    justifyContent: 'flex-end',
+    // marginTop: Platform.OS === 'ios' ? 30 : 25,
+  },
+  addButton: {
+    position: 'absolute',
+    right: 30,
+    bottom: 30,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: commonStyles.colors.today,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+
+})
---
+++ src/screens/Auth.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import { View,
+         ImageBackground,
+         Text,
+         StyleSheet,
+         TextInput,
+         TouchableOpacity } from 'react-native';
+
+import backgroundImage from '../../assets/imgs/login.jpg';
+import commonStyles from '../commonStyles';
+
+export default class Auth extends Component {
+
+    state = {
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        stageNew: true
+    }
+
+    render() {
+        return (
+            <ImageBackground source={backgroundImage} style={styles.background}>
+                <Text style={styles.title}>Tasks</Text>
+                <View style={styles.formContainer}>
+
+                    <Text style={styles.subTitle}>
+                        {this.state.stageNew ? 'Crie sua conta' : 'Informe seus dados'}
+                    </Text>
+
+                    {this.state.stageNew && 
+                        <TextInput placeholder='Nome' 
+                                value={this.state.name} 
+                                style={styles.input}
+                                onChangeText={name => this.setState({ name })} />                    
+                    }
+
+                    <TextInput placeholder='E-mail' 
+                               value={this.state.email} 
+                               style={styles.input}
+                               onChangeText={email => this.setState({ email })} />
+
+                    <TextInput placeholder='Senha' 
+                               secureTextEntry={true}
+                               value={this.state.password} 
+                               style={styles.input}
+                               onChangeText={password => this.setState({ password })} />   
+
+                    {this.state.stageNew && 
+                        <TextInput placeholder='Confirmação de Senha' 
+                                   value={this.state.confirmPassword} 
+                                   style={styles.input}
+                                   onChangeText={confirmPassword => this.setState({ confirmPassword })} />                    
+                    }
+
+                    <TouchableOpacity>
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>
+                                {this.state.stageNew ? 'Registrar' : 'Entrar'}
+                            </Text>
+                        </View>
+                    </TouchableOpacity>                            
+
+                </View>
+            </ImageBackground>
+        );
+    }
+
+}
+
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        color: commonStyles.colors.secondary,
+        fontSize: 70,
+        marginBottom: 10
+    },
+    subTitle: {
+        color: commonStyles.colors.secondary,
+        fontSize: 20,
+        textAlign: 'center',
+        marginBottom: 10
+    },
+    formContainer: {
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        padding: 20,
+        width: '90%'   
+    },    
+    input: {
+        marginTop: 10,
+        padding: 5,
+        backgroundColor: '#FFF'
+    },
+    button: {
+        backgroundColor: '#080',
+        marginTop: 10,
+        padding: 10,
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 20,
+    }
+});
